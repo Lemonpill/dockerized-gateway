44ff6fea772c84f9a7172436a394690e8a11055d
lemonpill <valeri.varin@gmail.com>
2022-06-10 00:15:21 +0300

    remove unused settings

--- gateway/core/settings.py
+++ gateway/core/settings.py
@@ -3,8 +3,9 @@ from .schemas import ConfigSchema
 from .utils import load_config
 
 class Settings(pydantic.BaseSettings):
-	server_host: str = os.getenv("SERVER_HOST")
-	server_port: int = os.getenv("SERVER_PORT")
 	server_config: ConfigSchema = load_config(
 		os.getenv("CONFIG_FILE")
 	)
+	jwt_key: str = os.getenv("JWT_KEY")
+	jwt_alg: str = os.getenv("JWT_ALG")
+
