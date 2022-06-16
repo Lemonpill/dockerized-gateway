0d0784edb7f847f1b0f4a6cb4019f6c73e7a7c08
lemonpill <valeri.varin@gmail.com>
2022-06-16 10:46:13 +0300

    add sample output to all endpoints

--- profiles/main.py
+++ profiles/main.py
@@ -1,7 +1,5 @@
 import logging, fastapi, pydantic, os
 
-app = fastapi.FastAPI()
-profiles = []
 
 log_file = os.getenv("LOG_FILE")
 log_format = os.getenv("LOG_FORMAT")
@@ -11,31 +9,60 @@ logging.basicConfig(
 )
 
 
+app = fastapi.FastAPI()
+
+
 class ProfileSchema(pydantic.BaseModel):
 	f_name: str
 	l_name: str
 
-@app.get("/profiles")
-async def get_profiles():
-	global profiles
-	return profiles
 
-@app.post("/profiles", response_class=fastapi.Response, status_code=201)
-async def add_profile(data: ProfileSchema):
-	global profiles
-	profiles.append(data.dict())
+@app.get("/profiles")
+async def get_profiles(request: fastapi.Request):
+	return {
+		"service": "profiles",
+		"endpoint": "GET /profiles",
+		"authentication": "required",
+		"status": "success",
+		"current_user": request.headers["User"]
+	}
+
+@app.post("/profiles")
+async def add_profile(request: fastapi.Request):
+	return {
+		"service": "profiles",
+		"endpoint": "POST /profiles",
+		"authentication": "required",
+		"status": "success",
+		"current_user": request.headers["User"]
+	}
 
 @app.get("/profiles/{profile_id}")
-async def get_profile(profile_id: int):
-	global profiles
-	if 0 <= profile_id < len(profiles):
-		return profiles[profile_id]
-	raise fastapi.HTTPException(404)
+async def get_profile(request: fastapi.Request, profile_id: str):
+	return {
+		"service": "profiles",
+		"endpoint": f"GET /profiles/{profile_id}",
+		"authentication": "required",
+		"status": "success",
+		"current_user": request.headers["User"]
+	}
 
 @app.patch("/profiles/{profile_id}")
-async def update_profile(profile_id: int):
-	return "ok"
+async def update_profile(request: fastapi.Request, profile_id: str):
+	return {
+		"service": "profiles",
+		"endpoint":  f"PATCH /profiles/{profile_id}",
+		"authentication": "required",
+		"status": "success",
+		"current_user": request.headers["User"]
+	}
 
 @app.delete("/profiles/{profile_id}")
-async def delete_profile(profile_id: int):
-	return "ok"
+async def delete_profile(request: fastapi.Request, profile_id: str):
+	return {
+		"service": "profiles",
+		"endpoint":  f"DELETE /profiles/{profile_id}",
+		"authentication": "required",
+		"status": "success",
+		"current_user": request.headers["user"]
+	}
