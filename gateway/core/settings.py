a78dd40a850d70b6e6d25539ccda34fa1d0d8a2c
lemonpill <valeri.varin@gmail.com>
2022-06-16 22:50:54 +0300

    no change

--- gateway/core/settings.py
+++ gateway/core/settings.py
@@ -2,10 +2,9 @@ import os, pydantic
 from .schemas import ConfigSchema
 from .utils import load_config
 
-class Settings(pydantic.BaseSettings):
-	server_config: ConfigSchema = load_config(
-		os.getenv("CONFIG_FILE")
-	)
-	jwt_key: str = os.getenv("JWT_KEY")
-	jwt_alg: str = os.getenv("JWT_ALG")
 
+class Settings(pydantic.BaseSettings):
+    
+    server_config: ConfigSchema = load_config(os.getenv("CONFIG_FILE"))
+    jwt_key: str = os.getenv("JWT_KEY")
+    jwt_alg: str = os.getenv("JWT_ALG")
